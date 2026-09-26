4a8633aeb5105bf0402a304b52b4d93e9093dae8
irfan-fauzi <irfanwintech@yahoo.com>
2026-09-27 00:10:00 +0700

    stoty

--- src/data/config.js
+++ src/data/config.js
@@ -57,24 +57,24 @@ export const CONFIG = {
 
   story: [
     {
-      when: "2025",
+      when: "Oktober 2025",
       title: "Pertemuan",
-      text: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      text: "Tidak ada yang terjadi secara kebetulan di dunia ini. Semua sudah diatur dengan sangat indah oleh Allah SWT. Kami tidak pernah menyangka bahwa perkenalan dan pertemuan sederhana di tahun 2025 menjadi awal dari sebuah perjalaan panjang yang membawa kami sampai di titik ini.",
     },
     {
-      when: "2025",
-      title: "Memulai hubungan",
-      text: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      when: "Januari 2026",
+      title: "Pendekatan",
+      text: "Seiring berjalannya waktu, kedekatan itu tumbuh dengan sendirinya. Dari obrolan kecil yang sederhana, perlahan hadir rasa nyaman yang membuat kami ingin saling mengenal lebih jauh. Hingga akhirnya pada Januari 2026 kami memutuskan untuk malangkah dalam sebuah hubungan.",
     },
     {
-      when: "2026",
+      when: "Juli 2026",
       title: "Lamaran",
-      text: "Di hadapan keluarga tercinta, Irfan meminang Karlina dengan penuh kesungguhan.",
+      text: "Perjalaan kami bukan tanpa ujian dan cerita. Namun setiap proses yang dilewati justru membawa kami semakin yakin satu sama lain. Sampai pada akhirnya, pada Juli 2026 kami dipertemukan dalam sebuah ikatan yang penuh doa dan restu keluarga.",
     },
     {
-      when: "2027",
-      title: "Hari bahagia",
-      text: "Dengan restu orang tua, kami siap memulai perjalanan baru sebagai suami dan istri.",
+      when: "April 2027",
+      title: "Pernikahan",
+      text: "Kami percaya, bukan karena bertemu lalu berjodoh. Tetapi karena berjodohlah, maka Allah mempertemukan kami dengan cara terbaik-Nya. Dan dengan rasa syukur, kami memutuskan untuk mengikrarkan janji suci pernikahan pada 03 April 2027.",
     },
   ],
 
